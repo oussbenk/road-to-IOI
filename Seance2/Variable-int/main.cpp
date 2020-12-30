e03b0b870abeddd4424968d4f8cd865bdb792f06
Oussama Ben Khiroun <oussbenk@users.noreply.github.com>
2020-12-31 00:37:15 +0100

    int type operators

---
+++ Seance2/Variable-int/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y;
+
+    x = 9;
+    y = 2;
+
+    cout << "Le contenu de x est " << x << endl;
+    cout << "Le résultat de la division euclidiènne " << x/y << endl;
+    cout << "Le reste de la division euclidiènne " << x%y << endl;
+    cout << "La sommme est " << x+y << endl;
+    cout << "Le produit est " << x*y << endl;
+
+    return 0;
+}
