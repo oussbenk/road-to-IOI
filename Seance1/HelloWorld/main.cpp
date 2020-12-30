e836b2fa8e7de48f7cda316a9577d18b57360f69
Oussama Ben Khiroun <oussbenk@users.noreply.github.com>
2020-12-31 00:40:16 +0100

    hello world

---
+++ Seance1/HelloWorld/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    cout << "Saisir votre age" << endl;
+    cin >> age;
+    cout << "Votre age est " << age << " ans" << endl;
+
+    // Une autre façon pour faire un retour à la ligne avec '\n'
+    cout << "Bonjour\n";
+
+    return 0;
+}
